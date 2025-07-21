d5de55da9b1a8a7a2c90bc5d00c540790bc87519
LuaDlc <luadiaslc@gmail.com>
2025-07-21 20:01:08 -0300

    fix: connection settings with mysql

--- bin/base_backend.dart
+++ bin/base_backend.dart
@@ -1,14 +1,27 @@
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf/shelf.dart';
+import 'package:mysql1/mysql1.dart';
 
-void main(List<String> arguments) {
-  serve(
+void main(List<String> arguments) async {
+  final conn = await MySqlConnection.connect(
+    ConnectionSettings(
+      host: 'localhost',
+      port: 3306,
+      user: 'dart_user',
+      db: 'delivery',
+      password: 'root',
+    ),
+  );
+  await conn.query("insert into tb_permission values ('ADMIN', 'A')");
+  print(await conn.query('select * from tb_permissions'));
+
+  await serve(
     (Request req) => Response(
       200,
       body: ' mundo',
       headers: {'content-type': 'application/json'},
     ),
     'localhost',
-    8080,
+    3306,
   );
 }
