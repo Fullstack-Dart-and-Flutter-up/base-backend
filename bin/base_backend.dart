1ca9aeb5978960f015c7de777f4541a1d7c2f8b0
LuaDlc <luadiaslc@gmail.com>
2025-07-24 15:59:54 -0300

    conexao com a base de dados MySQl

--- bin/base_backend.dart
+++ bin/base_backend.dart
@@ -1,15 +1,18 @@
-// import 'package:shelf/shelf_io.dart';
 // import 'package:shelf/shelf.dart';
 import 'package:commons_core/commons_core.dart';
+import 'core/database/database_mysql_adapter.dart';
 
 void main(List<String> arguments) async {
-  await CustomEnv.fromFile('.env');
+  CustomEnv.fromFile('.env');
   var result = await CustomEnv.get<String>(key: 'chave');
   print(result);
-  // serve(
+
+  print(await DatabaseMysqlAdapter().query('select * from tb_permissions'));
+
+  // await serve(
   //   (Request req) => Response(
   //     200,
-  //     body: ' mundo',
+  //     body: 'Ola mundo',
   //     headers: {'content-type': 'application/json'},
   //   ),
   //   'localhost',
